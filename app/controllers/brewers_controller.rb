6dba433e60c7ae9bd4f4ea7faf9384daaf54dd23
Aafke Smal <aafke.smal@gmail.com>
2016-07-26 16:57:39 +0200

    Find closest brewery with postal code

--- app/controllers/brewers_controller.rb
+++ app/controllers/brewers_controller.rb
@@ -1,11 +1,31 @@
 class BrewersController < ApplicationController
+  before_action :find_brewery, only: [:show]
+  before_action :show_nearby_brewery, only: [:index, :show]
 
   def index
-    @breweries = Brewer.all
+
   end
 
   def show
-    @brewery = Brewer.find(params[:id])
+
   end
+
+  private
+
+  def find_brewery
+     @brewery = Brewer.find(params[:id])
+     # @zipcode = params[:zipcode]
+  end
+
+  def show_nearby_brewery
+    @breweries = Brewer.all
+    @zipcode = params[:zipcode]
+    if @zipcode
+      @brewery = @breweries.near(@zipcode, 100, order: "distance").first
+    end
+  end
+
+
+
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+
+# def home
+#   @postcode = ""
+# end
+
+
+
 end
